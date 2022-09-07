9a8d259874c7f84332c905daeb7c581f4e9d2c5a
laineyhm <laineyhm@gmail.com>
2022-09-07 14:10:06 +0700

    working media recorder object

--- src/angular-app/bellows/shared/audio-recorder/audio-recorder.component.ts
+++ src/angular-app/bellows/shared/audio-recorder/audio-recorder.component.ts
@@ -1,24 +1,32 @@
 import * as angular from 'angular';
-import * as lamejs from 'lamejs';
+import {ChangeDetectorRef} from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
 
-declare var webkitAudioContext: {
-  new(): AudioContext;
-};
+declare var MediaRecorder: any;
 
 export class AudioRecorderController implements angular.IController {
 
-  static $inject = ['$interval', '$scope'];
-  constructor(private $interval: angular.IIntervalService, private $scope: angular.IScope) {}
+  static $inject = ['$interval'];
+
+  mediaRecorder:any;
+  chunks: any = [];
+	audioFiles: any = [];
+  constructor(private $interval: angular.IIntervalService, private cd: ChangeDetectorRef, private dom: DomSanitizer) {}
 
   isRecording = false;
   hasRecorded = false;
+
   audioSrc: string;
   blob: Blob;
   recordingTime: string;
+
   errorMessage: string;
+
   stopMediaStream: () => void;
   callback: (blob: Blob) => void;
+
   interval: angular.IPromise<void>;
+
 
   toggleRecording() {
     if (this.isRecording) this.stopRecording();
@@ -49,33 +57,29 @@ export class AudioRecorderController implements angular.IController {
 
     navigator.mediaDevices.getUserMedia({audio: true}).then(stream => {
 
-      this.$scope.$apply(() => {
-        this.hasRecorded = true;
-        this.errorMessage = null;
-        this.isRecording = true;
-      });
+      this.hasRecorded = true;
+      this.errorMessage = null;
+      this.isRecording = true;
 
       const recordingStartTime = new Date();
-      // webkit prefix required for Safari
-      const context = (window as any).AudioContext ? new AudioContext() : new webkitAudioContext();
-      const bufferSize = 0;
-      const channels = 1;
-      const processor = context.createScriptProcessor(bufferSize, channels, channels);
-      context.createMediaStreamSource(stream).connect(processor);
-      processor.connect(context.destination);
-      const sampleRate = context.sampleRate;
-      const bitrate = 128;
-      const mp3Encoder = new MP3Encoder(channels, sampleRate, bitrate);
 
-      function handleAudioData(event: AudioProcessingEvent) {
-        mp3Encoder.appendData(event.inputBuffer.getChannelData(0));
-      }
-      processor.addEventListener('audioprocess', handleAudioData);
+      this.mediaRecorder = new MediaRecorder(stream);
+      //What to do when this.mediaRecorder.stop() is called in the future
+      this.mediaRecorder.onstop = () => {
+        console.log('data available after MediaRecorder.stop() called.');
 
-      mp3Encoder.onMp3Blob(blob => {
-        this.blob = blob;
-        this.audioSrc = URL.createObjectURL(blob);
-      });
+        this.blob = new Blob(this.chunks, {type: 'audio/ogg; codecs=opus'});
+            this.chunks = [];
+            this.audioSrc = URL.createObjectURL(this.blob);
+            // audio.src = audioURL;
+            this.audioFiles.push(this.dom.bypassSecurityTrustUrl(this.audioSrc));
+            console.log(this.audioSrc);
+            console.log('recorder stopped');
+            this.cd.detectChanges();
+          };
+
+
+      this.mediaRecorder.start();
 
       this.interval = this.$interval(() => {
         const seconds = Math.floor((new Date().getTime() - recordingStartTime.getTime()) / 1000);
@@ -83,80 +87,27 @@ export class AudioRecorderController implements angular.IController {
       }, 1000);
 
       this.stopMediaStream = () => {
-        processor.removeEventListener('audioprocess', handleAudioData);
-        mp3Encoder.end();
         stream.getAudioTracks()[0].stop();
       };
 
     }, err => {
-      this.$scope.$apply(() => {
-        this.errorMessage = 'Unable to record audio from your microphone.';
-        this.isRecording = false;
-        this.hasRecorded = false;
-      });
+
+      this.errorMessage = 'Unable to record audio from your microphone.';
+      this.isRecording = false;
+      this.hasRecorded = false;
+
       console.error(err);
     });
   }
 
+
   private stopRecording() {
     if (this.interval) this.$interval.cancel(this.interval);
     if (this.stopMediaStream) this.stopMediaStream();
-  }
-
-}
-
-export class MP3Encoder {
-
-  constructor(private channels: number, private sampleRate: number, private bitrate: number) {}
-
-  buffer: Float32Array[] = [];
-  mp3BlobListeners: Array<(data: Blob) => void> = [];
-
-  lame = new lamejs.Mp3Encoder(this.channels, this.sampleRate, this.bitrate);
-
-  appendData(data: Float32Array) {
-    // copy the data, because Chromium 66.0.3359.139 overwrites it (FF 60.0.1 does not)
-    this.buffer.push(data.slice(0, data.length));
-  }
-
-  end() {
-    // Flatten the buffer array while converting it from Float32 to Int16
-    const pcmData = new Int16Array(this.buffer.length * this.buffer[0].length);
-
-    // max and min 16-bit values
-    const max = 0x7FFF;
-    const min = 0x8000;
-    let index = 0;
-    for (let i = 0, len1 = this.buffer.length; i < len1; ++i) {
-      const chunk = this.buffer[i];
-      for (let j = 0, len2 = chunk.length; j < len2; ++j) {
-        pcmData[index] = chunk[j] < 0 ? chunk[j] * min : chunk[j] * max;
-        ++index;
-      }
+    this.mediaRecorder.stop();
     }
 
-    const blockSize = 1152;
-    const mp3Data: Int8Array[] = [];
-
-    let encoded: Int8Array;
-    for (let i = 0; i < pcmData.length; i += blockSize) {
-      const chunk = pcmData.subarray(i, i + blockSize);
-      // This line takes the most time of this function, about 90%
-      encoded = this.lame.encodeBuffer(chunk);
-      if (chunk.length > 0) mp3Data.push(new Int8Array(encoded));
-    }
-
-    encoded = this.lame.flush();
-    if (encoded.length > 0) mp3Data.push(new Int8Array(encoded));
-    const blob = new Blob(mp3Data, {type: 'audio/mp3'});
-
-    this.mp3BlobListeners.forEach(cb => cb(blob));
   }
-
-  onMp3Blob(cb: (data: Blob) => void) {
-    this.mp3BlobListeners.push(cb);
-  }
-}
 
 export const AudioRecorderComponent: angular.IComponentOptions = {
   bindings: {
